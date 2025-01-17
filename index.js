8cd94d93ed5571696f53aa39885b2b62336c5f00
Douglas Luiz L <douglasluizleandro@gmail.com>
2025-01-16 23:55:33 -0300

    testenovo

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
-let lv= [
-    ["Ferro"<=999]
-    ["Bronze">=1000]
-    ["Prata">=2000]
-    ["Ouro">=5000]
-    ["Platina">=7000]
-    ["Ascendente">=8000]
-    ["Imortal">=9000]
-    ["Radiante">=10000]
-    ]
 let Heroi= [
-    ["Enoque", (lv=9200)]
-    ["Moises", (lv=8700)] 
-    ["Messias",(lv=99999)]
-    ]
-console.log(Heroi[1])
+    ["Enoque"]
+["Moises"]
+["Mesias"]
+]
+console.log (Heroi[1])
+    let lv=8700 
+switch (lv){
+    case ( lv <=999 ):
+    console.log ("O Heroi"+ Heroi + "esta no nivel Ferro");
+    break
+    case ( lv >=1000 ):
+        console.log ("O Heroi"+ Heroi + "esta no nivel Bronze");
+        break
+        case ( lv >=2000 ):
+            console.log ("O Heroi"+ Heroi + "esta no nivel Prata");
+            break
+            case ( lv >=5000 ):
+            console.log ("O Heroi"+ Heroi + "esta no nivel Ouro");
+            break
+            case ( lv >=7000 ):
+                console.log ("O Heroi"+ Heroi + "esta no nivel Platina");
+                break
+                case ( lv >=8000 ):
+                console.log ("O Heroi"+ Heroi + "esta no nivel Ascendente");
+                break
+                case ( lv >=9000 ):
+                console.log ("O Heroi"+ Heroi + "esta no nivel Imortal");
+                break
+                default:
+                case ( lv >=10000 ):
+                console.log ("O Heroi"+ Heroi + "esta no nivel Radiante");
+                
+}
