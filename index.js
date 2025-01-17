8e30e6cc1208bded9b4321a5c50ca9e4e6b540e3
Douglas Luiz L <douglasluizleandro@gmail.com>
2025-01-16 23:53:53 -0300

    novoteste

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
+let lv= [
+    ["Ferro"<=999]
+    ["Bronze">=1000]
+    ["Prata">=2000]
+    ["Ouro">=5000]
+    ["Platina">=7000]
+    ["Ascendente">=8000]
+    ["Imortal">=9000]
+    ["Radiante">=10000]
+    ]
 let Heroi= [
-    ["Enoque", lv=9200]
-    ["Moises", lv=8700] 
-    ["Messias",lv=99999]
-];
-let Lv= [
-    ["Ferro"<999]
-    ["Bronze">1000]
-    ["Prata">2000]
-    ["Ouro">5000]
-    ["Platina">7000]
-    ["Ascendente">8000]
-    ["Imortal">9000]
-    ["Radiante">10000]
-
-]
-console.log (Heroi(1)+" Esta no nivel "+lv)
-
+    ["Enoque", (lv=9200)]
+    ["Moises", (lv=8700)] 
+    ["Messias",(lv=99999)]
+    ]
+console.log(Heroi[1])
